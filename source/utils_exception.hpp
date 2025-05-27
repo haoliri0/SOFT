571d9a0d5e051d2a831bee74b5b3c58cd5358c4a
keli <zhengkeli2009@126.com>
2025-05-27 19:34:02 +0800

    增加了 `CudaException` 和 `cudaCheck()`

---
+++ source/utils_exception.hpp
@@ -0,0 +1,29 @@
+#ifndef STN_CUDA_CHECK_CUH
+#define STN_CUDA_CHECK_CUH
+
+#include<exception>
+#include<cuda_runtime.h>
+
+class CudaException final : public std::exception {
+public:
+    const cudaError_t cudaError;
+
+    explicit CudaException(const cudaError_t cudaError) : cudaError(cudaError) {}
+
+    [[nodiscard]]
+    const char *what() const noexcept override {
+        return cudaGetErrorString(cudaError);
+    }
+};
+
+static
+void cudaCheck(const cudaError_t error) {
+    if (error != cudaSuccess) throw CudaException(error);
+}
+
+static
+void cudaCheck() {
+    cudaCheck(cudaPeekAtLastError());
+}
+
+#endif
