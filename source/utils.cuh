40b18c32f2f9144f4a04e778d943a30a00016c85
keli <zhengkeli2009@126.com>
2025-05-27 18:13:46 +0800

    增加了 utils.cuh

---
+++ source/utils.cuh
@@ -0,0 +1,42 @@
+#ifndef STN_CUDA_UTILS_CUH
+#define STN_CUDA_UTILS_CUH
+
+#include <cuda_runtime.h>
+
+static __device__
+unsigned int get_blocks_n() {
+    return gridDim.x;
+}
+
+static __device__
+unsigned int get_block_i() {
+    return blockIdx.x;
+}
+
+static __device__
+unsigned int get_block_thread_i() {
+    return threadIdx.x;
+}
+
+static __device__
+unsigned int get_block_threads_n() {
+    return blockDim.x;
+}
+
+static __device__
+unsigned int get_global_thread_i() {
+    return get_block_threads_n() * get_block_i() + get_block_thread_i();
+}
+
+static __device__
+unsigned int get_global_threads_n() {
+    return get_block_threads_n() * get_blocks_n();
+}
+
+template<typename Integer>
+static __device__ __host__
+Integer ceiling_divide(Integer x, Integer y) {
+    return (x + y - 1) / y;
+}
+
+#endif
