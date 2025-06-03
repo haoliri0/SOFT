975a84fdd04879a85a3454364f2c338686bbc831
keli <zhengkeli2009@126.com>
2025-06-03 16:55:29 +0800

    增加了 cuda_dims_xor_xor 并在 Simulator::apply_y 中使用

--- source/gates.cu
+++ source/gates.cu
@@ -18,13 +18,19 @@ cudaError_t Simulator::apply_x(const int qubit) const noexcept {
 }
 
 cudaError_t Simulator::apply_y(const int qubit) const noexcept {
-    cudaError_t err = cudaSuccess;
-    err = this->apply_x(qubit);
-    if (err != cudaSuccess) return err;
-    err = this->apply_z(qubit);
-    if (err != cudaSuccess) return err;
-    // phase is ignored
-    return err;
+    const Qid qubits_n = this->qubits_n;
+    const Qid rows_n = 2 * qubits_n;
+    const Qid cols_n = 2 * qubits_n + 1;
+
+    const Qid src0_row = qubit;
+    const Qid src1_row = this->qubits_n + qubit;
+    const Qid dst_row = this->qubits_n * 2;
+
+    cuda_dims_xor_xor(
+        this->stream, this->table,
+        src0_row, src1_row, dst_row,
+        Dim{this->shots_n}, Dim{rows_n}, Dim{cols_n, 0, 1});
+    return cudaSuccess;
 }
 
 cudaError_t Simulator::apply_z(const int qubit) const noexcept {
