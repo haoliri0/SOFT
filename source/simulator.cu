bf67899872b22a6e3ba8a306167784d251e83f2d
keli <zhengkeli2009@126.com>
2025-06-11 16:56:12 +0800

    用新版 dimsop 重新实现了 `cuda_initialize_shots_table()`

--- source/simulator.cu
+++ source/simulator.cu
@@ -1,46 +1,35 @@
 #include <cuda_runtime.h>
 #include "./simulator.hpp"
-#include "./utils/thread.cuh"
+#include "./utils/dimsop.cuh"
 
 using namespace StnCuda;
 
-static __global__
-void kernel_initialize_shots_table(
+static __device__
+void initialize_table_op(const ShotsStatePtr shots_state_ptr, const DimsIdx<2> dims_idx) {
+    const Sid shot_i = dims_idx.get<0>();
+    const Qid row_i = dims_idx.get<1>();
+
+    Bit *ptr = shots_state_ptr
+        .get_shot_state_ptr(shot_i)
+        .get_table_ptr()
+        .get_row_ptr(row_i)
+        .get_pauli_ptr()
+        .get_ptr(row_i);
+
+    *ptr = true;
+}
+
+static
+void cuda_initialize_shots_table(
+    cudaStream_t const stream,
     ShotsStatePtr const shots_state_ptr
 ) {
     const CudaSid shots_n = shots_state_ptr.shots_n;
     const CudaQid qubits_n = shots_state_ptr.qubits_n;
     const CudaQid rows_n = 2 * qubits_n;
 
-    const unsigned int global_threads_n = shots_n * rows_n;
-    const unsigned int global_thread_i = get_global_thread_i();
-    if (global_thread_i >= global_threads_n) return;
-
-    const Sid shot_i = global_thread_i / rows_n;
-    const Qid row_i = global_thread_i % rows_n;
-
-    const ShotStatePtr shot_state_ptr = shots_state_ptr.get_shot_state_ptr(shot_i);
-    const TablePtr table_ptr = shot_state_ptr.get_table_ptr();
-    const TableRowPtr table_row_ptr = table_ptr.get_row_ptr(row_i);
-    const PauliRowPtr pauli_row_ptr = table_row_ptr.get_pauli_ptr();
-
-    *pauli_row_ptr.get_ptr(row_i) = true;
-}
-
-static
-cudaError_t cuda_initialize_shots_table(
-    ShotsStatePtr const shots_state_ptr,
-    cudaStream_t const stream
-) {
-    const CudaSid shots_n = shots_state_ptr.shots_n;
-    const CudaQid qubits_n = shots_state_ptr.qubits_n;
-    const CudaQid rows_n = 2 * qubits_n;
-
-    const unsigned int global_threads_n = shots_n * rows_n;
-    const unsigned int block_threads_n = default_block_threads_n;
-    const unsigned int blocks_n = ceiling_divide(global_threads_n, block_threads_n);
-    kernel_initialize_shots_table<<<blocks_n,block_threads_n,0,stream>>>(shots_state_ptr);
-    return cudaSuccess;
+    cuda_dims_op<ShotsStatePtr, 2, initialize_table_op>
+        (stream, shots_state_ptr, shots_n, rows_n);
 }
 
 cudaError_t Simulator::create(Sid const shots_n, Qid const qubits_n, Aid const map_limit) noexcept {
@@ -60,8 +49,7 @@ cudaError_t Simulator::create(Sid const shots_n, Qid const qubits_n, Aid const m
         // initialize state
         err = cudaMemsetAsync(this->shots_state_ptr.ptr, 0, state_bytes_n, this->stream);
         if (err != cudaSuccess) break;
-        err = cuda_initialize_shots_table(this->shots_state_ptr, this->stream);
-        if (err != cudaSuccess) break;
+        cuda_initialize_shots_table(this->stream, this->shots_state_ptr);
 
         // wait for async operations to complete
         err = cudaStreamSynchronize(this->stream);
