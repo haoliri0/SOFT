4df90ee2641b297fdbf7f06c57cc9adc7c1f127f
keli <zhengkeli2009@126.com>
2025-06-11 16:49:31 +0800

    重写了 `dimsop` 部分

--- source/utils/dimsop.cuh
+++ source/utils/dimsop.cuh
@@ -3,24 +3,34 @@
 
 #include "./thread.cuh"
 
-struct Dim {
-    unsigned int size;
-    unsigned int head = 0;
-    unsigned int tail = size;
-    unsigned int stride = 1;
+template<unsigned int dims_n>
+struct DimsIdx {
+    unsigned int idx;
+    DimsIdx<dims_n - 1> sub_dims_idx;
+
+    static
+    __device__ __host__
+    unsigned int n() {
+        return dims_n;
+    }
+
+    template<unsigned int di>
+    __device__ __host__
+    unsigned int get() const {
+        if constexpr (di == 0)
+            return idx;
+        else
+            return sub_dims_idx.template get<di - 1>();
+    }
 };
 
-
-static __device__ __host__
-unsigned int compute_dims_items_n() {
-    return 1;
-}
-
-template<typename... Dims>
-static __device__ __host__
-unsigned int compute_dims_items_n(const Dim dim, Dims... dims) {
-    return dim.size * compute_dims_items_n(dims...);
-}
+template<>
+struct DimsIdx<0> {
+    static
+    unsigned int n() {
+        return 0;
+    }
+};
 
 
 static __device__ __host__
@@ -28,152 +38,46 @@ unsigned int compute_dims_threads_n() {
     return 1;
 }
 
+template<typename... Dims>
 static __device__ __host__
-unsigned int compute_dim_threads_n(const Dim dim) {
-    return (dim.stride - 1 + dim.tail - dim.head) / dim.stride;
+unsigned int compute_dims_threads_n(const unsigned int dim, Dims... dims) {
+    return dim * compute_dims_threads_n(dims...);
+}
+
+
+static __device__ __host__
+DimsIdx<0> compute_dims_idx(const unsigned int) {
+    return DimsIdx<0>{};
 }
 
 template<typename... Dims>
 static __device__ __host__
-unsigned int compute_dims_threads_n(const Dim dim, Dims... dims) {
-    return compute_dim_threads_n(dim) * compute_dims_threads_n(dims...);
-}
-
-
-static __device__ __host__
-unsigned int compute_dims_offset(const unsigned int) {
-    return 0;
-}
-
-template<typename... Dims>
-static __device__ __host__
-unsigned int compute_dims_offset(const unsigned int thread_i, const Dim dim, Dims... dims) {
-    const unsigned int sub_items_n = compute_dims_items_n(dims...);
+DimsIdx<1 + sizeof...(Dims)> compute_dims_idx(const unsigned int thread_i, const unsigned int, Dims... dims) {
     const unsigned int sub_threads_n = compute_dims_threads_n(dims...);
     const unsigned int cur_thread_i = thread_i / sub_threads_n;
     const unsigned int sub_thread_i = thread_i % sub_threads_n;
-    const unsigned int cur_offset = (dim.head + cur_thread_i * dim.stride) * sub_items_n;
-    const unsigned int sub_offset = compute_dims_offset(sub_thread_i, dims...);
-    return cur_offset + sub_offset;
+    const auto sub_dims_idx = compute_dims_idx(sub_thread_i, dims...);
+    return DimsIdx<1 + sizeof...(Dims)>{cur_thread_i, sub_dims_idx};
 }
 
 
-template<typename Value, typename Args, void (*op)(Value *values, Args args), typename... Dims>
+template<typename Args, unsigned int dims_n, void (*op)(Args args, DimsIdx<dims_n> dims_idx), typename... Dims>
 static __global__
-void kernel_dims_op(Value *values, Args args, Dims... dims) {
-    const unsigned int global_thread_i = get_global_thread_i();
+void kernel_dims_op(Args args, Dims... dims) {
     const unsigned int global_threads_n = compute_dims_threads_n(dims...);
-    if (global_thread_i >= global_threads_n) return;
-    const unsigned int offset = compute_dims_offset(global_thread_i, dims...);
-    op(values + offset, args);
+    const unsigned int global_thread_i = get_global_thread_i();
+    if (global_thread_i >= global_threads_n)return;
+    op(args, compute_dims_idx(global_thread_i, dims...));
 }
 
-template<typename Value, typename Args, void (*op)(Value *values, Args args), typename... Dims>
+template<typename Args, unsigned int dims_n, void (*op)(Args args, DimsIdx<dims_n> dims_idx), typename... Dims>
 static __host__
-void cuda_dims_op(cudaStream_t stream, Value *values, Args args, Dims... dims) {
+void cuda_dims_op(cudaStream_t stream, Args args, Dims... dims) {
     const unsigned int global_threads_n = compute_dims_threads_n(dims...);
     const unsigned int block_threads_n = default_block_threads_n;
     const unsigned int blocks_n = ceiling_divide(global_threads_n, block_threads_n);
-    kernel_dims_op<Value, Args, op, Dims...>
-        <<<blocks_n, block_threads_n, 0 ,stream>>>(values, args, dims...);
-}
-
-
-template<typename Value>
-static __device__ __host__
-void op_fill(Value *values, Value value) {
-    *values = value;
-}
-
-template<typename Value, typename... Dims>
-static __host__
-void cuda_dims_fill(
-    cudaStream_t stream,
-    Value *values,
-    Value value,
-    Dims... dims
-) {
-    cuda_dims_op<Value, Value, op_fill<Value>>(stream, values, value, dims...);
-}
-
-
-struct Op2Args {
-    unsigned int p0;
-    unsigned int p1;
-};
-
-template<typename Value, void (*op)(Value &v0, Value &v1)>
-static __device__ __host__
-void op2(Value *values, Op2Args args) {
-    op(values[args.p0], values[args.p1]);
-}
-
-template<typename Value, void (*op)(Value &v0, Value &v1), typename... Dims>
-static __host__
-void cuda_dims_op2(
-    cudaStream_t stream,
-    Value *values,
-    const unsigned int p0,
-    const unsigned int p1,
-    Dims... dims
-) {
-    cuda_dims_op<Value, Op2Args, op2<Value, op>>(stream, values, Op2Args{p0, p1}, dims...);
-}
-
-
-struct Op3Args {
-    unsigned int p0;
-    unsigned int p1;
-    unsigned int p2;
-};
-
-template<typename Value, void (*op)(Value &v0, Value &v1, Value &v2)>
-static __device__ __host__
-void op3(Value *values, Op3Args args) {
-    op(values[args.p0], values[args.p1], values[args.p2]);
-}
-
-template<typename Value, void (*op)(Value &v0, Value &v1, Value &v2), typename... Dims>
-static __host__
-void cuda_dims_op3(
-    cudaStream_t stream,
-    Value *values,
-    const unsigned int p0,
-    const unsigned int p1,
-    const unsigned int p2,
-    Dims... dims
-) {
-    cuda_dims_op<Value, Op3Args, op3<Value, op>>(stream, values, Op3Args{p0, p1, p2}, dims...);
-}
-
-
-struct Op5Args {
-    unsigned int p0;
-    unsigned int p1;
-    unsigned int p2;
-    unsigned int p3;
-    unsigned int p4;
-};
-
-template<typename Value, void (*op)(Value &v0, Value &v1, Value &v2, Value &v3, Value &v4)>
-static __device__ __host__
-void op5(Value *values, Op5Args args) {
-    op(values[args.p0], values[args.p1], values[args.p2], values[args.p3], values[args.p4]);
-}
-
-template<typename Value, void (*op)(Value &v0, Value &v1, Value &v2, Value &v3, Value &v4), typename... Dims>
-static __host__
-void cuda_dims_op5(
-    cudaStream_t stream,
-    Value *values,
-    const unsigned int p0,
-    const unsigned int p1,
-    const unsigned int p2,
-    const unsigned int p3,
-    const unsigned int p4,
-    Dims... dims
-) {
-    cuda_dims_op<Value, Op5Args, op5<Value, op>>(stream, values, Op5Args{p0, p1, p2, p3, p4}, dims...);
+    kernel_dims_op<Args, dims_n, op, Dims...>
+        <<<blocks_n, block_threads_n, 0 ,stream>>>(args, dims...);
 }
 
 #endif
