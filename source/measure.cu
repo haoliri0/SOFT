31f19596693281d29374bad507116915384317b3
keli <zhengkeli2009@126.com>
2025-06-19 19:55:30 +0800

    优化了 `cuda_compute_decomp_pivot` 的参数结构，移除了未使用的 `target` 参数并简化了相关逻辑

--- source/measure.cu
+++ source/measure.cu
@@ -5,15 +5,11 @@
 
 using namespace StnCuda;
 
-struct ArgsComputeMeasureCondition {
-    ShotsStatePtr shots_state_ptr;
-    Qid target;
-};
 
 static __device__
-void op_compute_decomp_pivot(const ArgsComputeMeasureCondition args, const DimsIdx<1> dims_idx) {
+void op_compute_decomp_pivot(const ShotsStatePtr shots_state_ptr, const DimsIdx<1> dims_idx) {
     Sid const shot_i = dims_idx.get<0>();
-    const ShotStatePtr shot_state_ptr = args.shots_state_ptr.get_shot_state_ptr(shot_i);
+    const ShotStatePtr shot_state_ptr = shots_state_ptr.get_shot_state_ptr(shot_i);
     const DecompPtr decomp_ptr = shot_state_ptr.get_decomp_ptr();
     const Bit *decomp_bits = decomp_ptr.get_bits_ptr();
     Qid &decomp_pivot = *decomp_ptr.get_pivot_ptr();
@@ -32,17 +28,16 @@ void op_compute_decomp_pivot(const ArgsComputeMeasureCondition args, const DimsI
 static __host__
 void cuda_compute_decomp_pivot(
     cudaStream_t const stream,
-    ShotsStatePtr const shots_state_ptr,
-    const Qid target
+    ShotsStatePtr const shots_state_ptr
 ) {
     const Sid shots_n = shots_state_ptr.shots_n;
-    cuda_dims_op<ArgsComputeMeasureCondition, 1, op_compute_decomp_pivot>
-        (stream, {shots_state_ptr, target}, dimsof(shots_n));
+    cuda_dims_op<ShotsStatePtr, 1, op_compute_decomp_pivot>
+        (stream, shots_state_ptr, dimsof(shots_n));
 }
 
 void Simulator::measure(const Qid target, Bit *res, Flt *prob) const noexcept {
     cuda_compute_decomposed_bits(stream, shots_state_ptr, target);
-    cuda_compute_decomp_pivot(stream, shots_state_ptr, target);
+    cuda_compute_decomp_pivot(stream, shots_state_ptr);
 
     // TODO
 }
