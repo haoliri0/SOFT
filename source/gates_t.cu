63841de966e2ba02eb01046884fb8f90eb1a871b
keli <zhengkeli2009@126.com>
2025-06-23 13:57:42 +0800

    将 `bits_to_int()` 和 `compute_sign()` 移到 `decompose.cuh`

--- source/gates_t.cu
+++ source/gates_t.cu
@@ -1,32 +1,9 @@
 #include "./simulator.hpp"
 #include "./decompose.hpp"
+#include "./decompose.cuh"
 #include "./dimsop.cuh"
 
 using namespace StnCuda;
-
-
-static __device__
-Aid bits_to_int(const Bit *bits, const unsigned int n) {
-    Aid v = 0;
-    for (unsigned int i = 0; i < n; i++) {
-        const Bit bit = bits[i];
-        v <<= 1;
-        v |= bit;
-    }
-    return v;
-}
-
-static __device__
-Bit compute_sign(Aid key, const Bit *stab_bits, const Qid qubits_n) {
-    Bit sign = false;
-    for (int qubit_i = 0; qubit_i < qubits_n; ++qubit_i) {
-        const Bit key_bit = key % 2;
-        const Bit stab_bit = stab_bits[qubit_i];
-        sign ^= stab_bit & key_bit;
-        key >>= 1;
-    }
-    return sign;
-}
 
 
 template<bool dagger>
