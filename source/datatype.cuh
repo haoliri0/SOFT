cfce01161b1f645239ad72757340ac63c55ef377
keli <zhengkeli2009@126.com>
2025-07-01 14:39:58 +0800

    增加了工具函数 `sign_to_flt()` 和 `phase_to_amp()`

--- source/datatype.cuh
+++ source/datatype.cuh
@@ -19,6 +19,26 @@ using Amp = cuda::std::complex<Flt>;
 
 constexpr Qid NullPivot = static_cast<Qid>(-1);
 
+static __device__ __host__
+Flt sign_to_flt(const Bit sign) {
+    return !sign ? 1 : -1;
+}
+
+static __device__ __host__
+Amp phase_to_amp(const Phs phase) {
+    if (!(phase / 2 % 2)) {
+        if (!(phase % 2))
+            return Amp{+1, 0}; // 0: +1
+        else
+            return Amp{0, +1}; // 1: +i
+    } else {
+        if (!(phase % 2))
+            return Amp{-1, 0}; // 2: -1
+        else
+            return Amp{0, -1}; // 3: -i
+    }
+}
+
 }
 
 #endif
