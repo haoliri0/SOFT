b1c269424f60ecfa4f9b5759ed88319999f79641
keli <zhengkeli2009@126.com>
2025-07-07 15:30:44 +0800

    优化了 `decompose.cu` 中的代码

--- source/decompose.cu
+++ source/decompose.cu
@@ -7,39 +7,37 @@
 using namespace StnCuda;
 
 
-struct ComputeDecomposedBitsArgs {
-    ShotsStatePtr ptr;
-    Qid target;
+struct ArgsComputeDecomposedBits {
+    const ShotsStatePtr shots_state_ptr;
+    const Qid target;
 };
 
 static __device__
-void op_compute_decomposed_bits(const ComputeDecomposedBitsArgs args, const DimsIdx<2> dims_idx) {
+void op_compute_decomposed_bits(const ArgsComputeDecomposedBits args, const DimsIdx<2> dims_idx) {
     Sid const shot_i = dims_idx.get<0>();
     Qid const row_i = dims_idx.get<1>();
-    Qid const qubits_n = args.ptr.qubits_n;
+    ShotsStatePtr const shots_state_ptr = args.shots_state_ptr;
+    ShotStatePtr const shot_state_ptr = shots_state_ptr.get_shot_ptr(shot_i);
+    Qid const qubits_n = shot_state_ptr.qubits_n;
     Qid const rows_n = 2 * qubits_n;
     Qid const target = args.target;
 
-    const PauliRowPtr pauli_row_ptr = args.ptr
-        .get_shot_ptr(shot_i)
+    const PauliRowPtr pauli_row_ptr = shot_state_ptr
         .get_table_ptr()
         .get_row_ptr(row_i)
         .get_pauli_ptr();
+    const DecompPtr decomp_ptr = shot_state_ptr
+        .get_decomp_ptr();
 
     // 在分解 Z 门时，根据 target 位置的门的类型
     // 判断某个 (de)stabilizer 是否应该存在
     const Bit x = *pauli_row_ptr.get_x_ptr(target);
     const Bit z = *pauli_row_ptr.get_z_ptr(target);
 
-    Bit *bits_ptr = args.ptr
-        .get_shot_ptr(shot_i)
-        .get_decomp_ptr()
-        .get_bits_ptr();
-
     // 注意，这里计算结果是反的
     // stabilizer 算出的结果放到 destabilizer bit
     // destabilizer 算出的结果放到 stabilizer bit
-    Bit &bit = *(bits_ptr + (row_i + qubits_n) % rows_n);
+    Bit &bit = *decomp_ptr.get_bit_ptr((row_i + qubits_n) % rows_n);
 
     const Bit isX = x && !z;
     const Bit isY = x && z;
@@ -55,19 +53,18 @@ void cuda_compute_decomposed_bits(
     const Sid shots_n = shots_state_ptr.shots_n;
     const Qid qubits_n = shots_state_ptr.qubits_n;
     const Qid rows_n = 2 * qubits_n;
-    cuda_dims_op<ComputeDecomposedBitsArgs, 2, op_compute_decomposed_bits>
+    cuda_dims_op<ArgsComputeDecomposedBits, 2, op_compute_decomposed_bits>
         (stream, {shots_state_ptr, target}, dimsof(shots_n, rows_n));
 }
 
 
 static __device__
 void op_compute_decomposed_phase(const ShotsStatePtr shots_state_ptr, const DimsIdx<1> dims_idx) {
-    Qid const qubits_n = shots_state_ptr.qubits_n;
-    Qid const rows_n = 2 * qubits_n;
-    Qid const cols_n = 2 * qubits_n;
-
     Sid const shot_i = dims_idx.get<0>();
     const ShotStatePtr shot_state_ptr = shots_state_ptr.get_shot_ptr(shot_i);
+    Qid const qubits_n = shot_state_ptr.qubits_n;
+    Qid const rows_n = 2 * qubits_n;
+    Qid const cols_n = 2 * qubits_n;
 
     const TablePtr table_ptr = shot_state_ptr.get_table_ptr();
     const DecompPtr decomp_ptr = shot_state_ptr.get_decomp_ptr();
@@ -117,11 +114,11 @@ void op_compute_decomp_pivot(const ShotsStatePtr shots_state_ptr, const DimsIdx<
     Sid const shot_i = dims_idx.get<0>();
     const ShotStatePtr shot_state_ptr = shots_state_ptr.get_shot_ptr(shot_i);
     const DecompPtr decomp_ptr = shot_state_ptr.get_decomp_ptr();
+    const Qid qubits_n = decomp_ptr.qubits_n;
     const Bit *decomp_bits = decomp_ptr.get_destab_bits_ptr();
     Qid &decomp_pivot = *decomp_ptr.get_pivot_ptr();
 
     decomp_pivot = NullPivot;
-    Qid const qubits_n = shot_state_ptr.qubits_n;
     for (Qid row_i = 0; row_i < qubits_n; ++row_i) {
         if (decomp_bits[row_i]) {
             decomp_pivot = row_i;
