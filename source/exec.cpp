66c6fee71b2f9a4cc35dfd0191a339b1b2585897
keli <zhengkeli2009@126.com>
2025-08-12 17:36:22 +0800

    增加了 READ 算子，删除了 flush 机制

--- source/exec.cpp
+++ source/exec.cpp
@@ -98,7 +98,8 @@ enum class ParseCircuitLineError {
     Success,
     IOError,
     IllegalOp,
-    IllegalArg
+    IllegalArg,
+    ExecFailed,
 };
 
 static
@@ -211,11 +212,68 @@ ParseCircuitLineError execute_op(
     return execute_op(istream, wrapped);
 }
 
+ParseCircuitLineError perform_read_op(
+    const Simulator &simulator,
+    const int result_i
+) {
+    const ShotsStatePtr shots_state_ptr = simulator.shots_state_ptr;
+    const Kid results_m = shots_state_ptr.results_m;
+    const Sid shots_n = shots_state_ptr.shots_n;
+
+    cudaError cuda_err = cudaStreamSynchronize(simulator.stream);
+    if (cuda_err != cudaSuccess) {
+        fprintf(stderr, "Error occurs when executing previous operations!\n");
+        fprintf(stderr, "%s\n%s\n", cudaGetErrorName(cuda_err), cudaGetErrorString(cuda_err));
+        return ParseCircuitLineError::ExecFailed;
+    }
+
+    for (Sid shot_i = 0; shot_i < shots_n; ++shot_i) {
+        const ShotStatePtr shot_state_ptr = shots_state_ptr.get_shot_ptr(shot_i);
+
+        Err error;
+        cuda_err = cudaMemcpy(&error, shot_state_ptr.get_results_ptr().get_error_ptr(),
+            sizeof(Err), cudaMemcpyDeviceToHost);
+        if (cuda_err != cudaSuccess) break;
+
+        Rvl results_value[results_m];
+        cuda_err = cudaMemcpy(results_value, shot_state_ptr.get_results_ptr().get_values_ptr(),
+            results_m * sizeof(Rvl), cudaMemcpyDeviceToHost);
+        if (cuda_err != cudaSuccess) break;
+
+        Flt results_prob[results_m];
+        cuda_err = cudaMemcpy(results_prob, shot_state_ptr.get_results_ptr().get_probs_ptr(),
+            results_m * sizeof(Flt), cudaMemcpyDeviceToHost);
+        if (cuda_err != cudaSuccess) break;
+
+        Rid result_j;
+        if (result_i < 0) {
+            Rid results_n;
+            cuda_err = cudaMemcpy(&results_n, shot_state_ptr.get_results_ptr().get_results_n_ptr(),
+                sizeof(Rid), cudaMemcpyDeviceToHost);
+            if (cuda_err != cudaSuccess) break;
+            result_j = (results_n + result_i) % results_m;
+        } else {
+            result_j = result_i % results_m;
+        }
+
+        const Rvl value = results_value[result_j];
+        const Flt prob = results_prob[result_j];
+        printf("%u,%u,%u,%f\n", shot_i, error, value, prob);
+    }
+
+    if (cuda_err != cudaSuccess) {
+        fprintf(stderr, "Error occurs when reading result %d!\n", result_i);
+        fprintf(stderr, "%s\n%s\n", cudaGetErrorName(cuda_err), cudaGetErrorString(cuda_err));
+        return ParseCircuitLineError::ExecFailed;
+    }
+
+    return ParseCircuitLineError::Success;
+}
+
 static
 ParseCircuitLineError execute_line(
     const Simulator &simulator,
-    std::istream &istream,
-    bool &hasResult
+    std::istream &istream
 ) noexcept {
     constexpr size_t name_limit = 16;
     char name[name_limit];
@@ -223,7 +281,6 @@ ParseCircuitLineError execute_line(
     if (istream.bad()) return ParseCircuitLineError::IOError;
     if (istream.eof()) return ParseCircuitLineError::Success;
 
-    hasResult = false;
     if (match(name, ""))
         return execute_op(istream, [] {});
     if (match(name, "X"))
@@ -245,7 +302,6 @@ ParseCircuitLineError execute_line(
     if (match(name, "CX"))
         return execute_op(istream, simulator, &Simulator::apply_cx);
 
-    hasResult = true;
     if (match(name, "M"))
         return execute_op(istream, simulator, &Simulator::apply_measure);
     if (match(name, "D"))
@@ -263,64 +319,12 @@ ParseCircuitLineError execute_line(
     if (match(name, "DEP2"))
         return execute_op(istream, simulator, &Simulator::apply_noise_depo2);
 
+    if (match(name, "READ"))
+        return execute_op(istream, std::function([simulator](const int result_i) {
+            return perform_read_op(simulator, result_i);
+        }));
+
     return ParseCircuitLineError::IllegalOp;
-}
-
-static
-cudaError flush_results(
-    const CliArgs &args,
-    const Simulator &simulator,
-    const size_t line_i,
-    const size_t lines_n,
-    const Rid result_i,
-    const Rid results_n
-) {
-    const ShotsStatePtr shots_state_ptr = simulator.shots_state_ptr;
-    const Kid results_m = shots_state_ptr.results_m;
-    const Sid shots_n = shots_state_ptr.shots_n;
-
-    cudaError cuda_err = cudaStreamSynchronize(simulator.stream);
-    if (cuda_err != cudaSuccess) {
-        fprintf(stderr, "Error occurs execute lines %lu:%lu\n", line_i + 1, lines_n + 1);
-        fprintf(stderr, "%s\n%s\n", cudaGetErrorName(cuda_err), cudaGetErrorString(cuda_err));
-        return cuda_err;
-    }
-
-    if (args.mode < 1)
-        return cuda_err;
-
-    for (Sid shot_i = 0; shot_i < shots_n; ++shot_i) {
-        const ShotStatePtr shot_state_ptr = shots_state_ptr.get_shot_ptr(shot_i);
-
-        Err error;
-        cuda_err = cudaMemcpy(&error, shot_state_ptr.get_results_ptr().get_error_ptr(),
-            sizeof(Err), cudaMemcpyDeviceToHost);
-        if (cuda_err != cudaSuccess) break;
-
-        Rvl results_value[results_m];
-        cuda_err = cudaMemcpy(results_value, shot_state_ptr.get_results_ptr().get_values_ptr(),
-            results_m * sizeof(Rvl), cudaMemcpyDeviceToHost);
-        if (cuda_err != cudaSuccess) break;
-
-        Flt results_prob[results_m];
-        cuda_err = cudaMemcpy(results_prob, shot_state_ptr.get_results_ptr().get_probs_ptr(),
-            results_m * sizeof(Flt), cudaMemcpyDeviceToHost);
-        if (cuda_err != cudaSuccess) break;
-
-        for (Rid result_j = result_i; result_j < results_n; ++result_j) {
-            const Rvl value = results_value[result_j % results_m];
-            const Flt prob = results_prob[result_j % results_m];
-            printf("%u,%u,%u,%f\n", shot_i, error, value, prob);
-        }
-    }
-
-    if (cuda_err != cudaSuccess) {
-        fprintf(stderr, "Error occurs when reading results %u:%u\n", result_i, results_n);
-        fprintf(stderr, "%s\n%s\n", cudaGetErrorName(cuda_err), cudaGetErrorString(cuda_err));
-        return cuda_err;
-    }
-
-    return cuda_err;
 }
 
 // main
@@ -360,30 +364,13 @@ int main(const int argc, const char **argv) {
         const clock_t time_start = clock();
 
         size_t lines_n = 0;
-        size_t lines_n_flushed = 0;
-        Rid results_n = 0;
-        Rid results_n_flushed = 0;
         std::istream &istream = std::cin;
         istream >> std::noskipws;
         while (istream.good()) {
-            bool hasResult = false;
-            line_err = execute_line(simulator, istream, hasResult);
+            line_err = execute_line(simulator, istream);
             if (line_err != ParseCircuitLineError::Success) {
                 fprintf(stderr, "Error occurs when parsing line %lu.\n", lines_n + 1);
                 break;
-            }
-
-            if (hasResult) {
-                results_n += 1;
-                if (args.mode >= 2 || results_n - results_n_flushed >= args.results_m) {
-                    cuda_err = flush_results(
-                        args, simulator,
-                        lines_n_flushed, lines_n,
-                        results_n_flushed, results_n);
-                    if (cuda_err != cudaSuccess) break;
-                    lines_n_flushed = lines_n;
-                    results_n_flushed = results_n;
-                }
             }
 
             if (args.mode >= 2)
@@ -391,17 +378,6 @@ int main(const int argc, const char **argv) {
 
             lines_n += 1;
         }
-
-        if (cuda_err != cudaSuccess)
-            break;
-
-        cuda_err = flush_results(
-            args, simulator,
-            lines_n_flushed, lines_n,
-            results_n_flushed, results_n);
-
-        if (cuda_err != cudaSuccess)
-            break;
 
         if (line_err != ParseCircuitLineError::Success)
             break;
