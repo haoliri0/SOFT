a4c853b2052f26d3d7b2ff7113d2f1b40f127a4d
keli <zhengkeli2009@126.com>
2025-08-27 19:18:46 +0800

    增加了 Simulator::apply_classical_controlled_x(), Simulator::apply_classical_controlled_y(), Simulator::apply_classical_controlled_z()

--- source/classical.cu
+++ source/classical.cu
@@ -1,5 +1,6 @@
 #include "./simulator.hpp"
 #include "./shotsop.cuh"
+#include "./gates.cuh"
 
 using namespace StnCuda;
 
@@ -50,4 +51,47 @@ void op_classical_not(const ShotStatePtr shot_state_ptr) {
 
 void Simulator::apply_classical_not() const noexcept {
     cuda_shots_op<op_classical_not>(stream, shots_state_ptr);
+}
+
+
+struct ArgsClassicalControlledGate1 {
+    ShotsStatePtr shots_state_ptr;
+    Qid target;
+};
+
+template<void (*op)(Bit &s, Bit &x, Bit &z)>
+static __device__
+void op_classical_controlled_gate1(const ArgsClassicalControlledGate1 args, const DimsIdx<2> dims_idx) {
+    Sid const shot_i = dims_idx.get<0>();
+    const ShotsStatePtr shots_state_ptr = args.shots_state_ptr;
+    const ShotStatePtr shot_state_ptr = shots_state_ptr.get_shot_ptr(shot_i);
+    const ResultsPtr results_ptr = shot_state_ptr.get_results_ptr();
+    const Bit cond = *results_ptr.get_work_value_ptr();
+    if (cond) op_apply_gate1<op>({shots_state_ptr, args.target}, dims_idx);
+}
+
+template<void (*op)(Bit &s, Bit &x, Bit &z)>
+static __host__
+void cuda_classical_controlled_gate1(
+    cudaStream_t const stream,
+    ShotsStatePtr const shots_state_ptr,
+    const Qid target
+) {
+    const Sid shots_n = shots_state_ptr.shots_n;
+    const Qid qubits_n = shots_state_ptr.qubits_n;
+    const Qid rows_n = 2 * qubits_n;
+    cuda_dims_op<ArgsClassicalControlledGate1, 2, op_classical_controlled_gate1<op>>
+        (stream, {shots_state_ptr, target}, dimsof(shots_n, rows_n));
+}
+
+void Simulator::apply_classical_controlled_x(const Qid target) const noexcept {
+    cuda_classical_controlled_gate1<op_apply_x>(stream, shots_state_ptr, target);
+}
+
+void Simulator::apply_classical_controlled_y(const Qid target) const noexcept {
+    cuda_classical_controlled_gate1<op_apply_y>(stream, shots_state_ptr, target);
+}
+
+void Simulator::apply_classical_controlled_z(const Qid target) const noexcept {
+    cuda_classical_controlled_gate1<op_apply_z>(stream, shots_state_ptr, target);
 }
